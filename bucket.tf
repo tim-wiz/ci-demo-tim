1ccd12e6fd1cff8f4e0e5196ec76da09897b12d8
tim-wiz <154359443+tim-wiz@users.noreply.github.com>
2025-08-25 15:01:11 -0600

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -46,10 +46,10 @@ output "bucket_name" {
   value = aws_s3_bucket.bad_bucket.bucket
 }
 
-# output "bucket_arn" {
-#   value = aws_s3_bucket.bad_bucket.arn
-# }
-
-output "bad_user_name" {
-  value = aws_iam_user.bad_user.name
+output "bucket_arn" {
+  value = aws_s3_bucket.bad_bucket.arn
 }
+
+# output "bad_user_name" {
+#   value = aws_iam_user.bad_user.name
+# }
